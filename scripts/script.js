9c7d050f8d11a159373e9e3fca834d2d58c6bf2e
samiam151 <samiam151@gmail.com>
2016-05-08 16:55:01 -0400

    Initial commit

---
+++ scripts/script.js
@@ -0,0 +1,48 @@
+// Make the home page height equal to the viewport
+$('.home').css('height', function(){
+    var winHeight = $(window).height(),
+        // padding = winHeight * 0.08;
+        padding = winHeight;
+    return winHeight;
+});
+
+// Modal
+var modalContent = $('.modalContent'),
+        modal = $('.modal');
+        
+$('.work li').each(function(){
+    
+
+    $(this).on('click', function(){
+        var arrayOffset = 1,
+            num = $(this).data('work-number'),
+            thisWork = works[num];
+        // console.log(thisWork);
+
+        // $('.modalCover').css('background-color', 'rgba(0,0,0,0.5)');
+        modal.css({
+            'display': 'block',
+            'opacity': '1'
+        });
+
+        // Populate the modal information
+        $('.modal-name').html(thisWork.name);
+        $('.modal-problem').html(thisWork.problem);
+        $('.modal-solution').html(thisWork.solution);
+        thisWork.client ? $('.modal-client').html(thisWork.client) : $('.modal-client').html('');
+        
+        // Add the pictures
+        thisWork.pictures.forEach(function(item){
+            $('.modal-pictures').append('<img src="' + item + '">');
+        });
+
+        // To run when the modal closes
+        $('.close').on('click', function(){
+            modal.css('display', 'none');
+            $('.modal-pictures').html(''); // clear the pictures div 
+        });
+
+    });
+});
+
+
---
+++ scripts/works.js
@@ -0,0 +1,50 @@
+var works = [
+    {
+        name: 'Typeface Book',
+        pictures: [
+            'assets/typo_book/expressive_type_cutting',
+            'assets/typo_book/expressive_type_water',
+            'assets/typo_book/type_book_binding'
+        ],
+        problem: 'Problem 1',
+        solution: 'Solution 1'
+    },
+    {
+        name: 'Gentleman Box',
+        pictures: [
+            'assets/gentleman/thumb.jpg',
+            'assets/gentleman/Bsam_explorationPt6-working2.jpg',
+            'assets/gentleman/gmm_color.jpg',
+            'assets/gentleman/gmm_logo.jpg',
+            'assets/gentleman/IMG_1546.jpg',
+            'assets/gentleman/IMG_1547.jpg',
+            'assets/gentleman/IMG_1548.jpg'
+        ],
+        problem: '<div><h3 class="objective">Objective:</h3><p>Work with a partner to explore and collaborate 3 unrelated words and find a design opportunity.</p><h4>Technical Requirements:</h4><p>Final pieces must be in the in the intended medium.</p></div>',
+        solution: '<div><h3>Creative Brief: </h3><p>During my play, exploration, and collaboration of my words “mirror, intestine, and vivid” I made a couple of pretty strong connections with the words and the one that I focused on the most was the concept of “Self” and then farther down that rabbit whole- the concept of “suicide”. Self presented itself with my three words differently, “Mirror & Self” showing or physical and exterior self, “Intestine & Self” showing the physical and internal, and lastly “Vivid & Self” showing our memories and imagination. With one of my words being “intestine” my lists and brain maps got graphic and dark very quickly,paired with the new concept of self, I landed on “Suicide”. </p><p>Once I arrived at a design opportunity, I began visual and factual research . Interestingly, the majority of the existing add campaigns, websites, and resources are geared toward young teenage girls. Even When I thought “suicide” I thought of that demographic first too. However, according to the American Foundation of Suicide Prevention 65% of suicides in the United States are committed by white males ages 45–59. I feel there is a design opportunity here and I choose to target my design toward these men.</p> <p>I believe there is a strong need for a message of awareness to this market mainly because the large numbers of suicide by adult males is a silent epidemic. I believe Bringing attention and awareness to this market will help change the discussion we have on suicide because it would focus on those who statistically need the help</p></div>'
+    },
+    {
+        // Receorder
+        name: 'Invitation',
+        pictures: [
+            'assets/recorder/160507_1423.jpg',
+            'assets/recorder/160507_1426.jpg',
+            'assets/recorder/160507_1451.jpg',
+            'assets/recorder/01.jpg',
+            'assets/recorder/04.jpg',
+            'assets/recorder/06.jpg',
+            'assets/recorder/recorder_inspiration1.jpg',
+            'assets/recorder/recorder_inspiration2.jpg'
+        ],
+        problem: "<div><h3 class='objective'>Objective:</h3><p>Conceptualize and develop designs for two separate event invitations of your choice. Each invitation must utilize different paper, have unique and complex die, matching envelope, and must meet the specified technical requirements.</p><h4>Technical Requirements:</h4><p>Each invitation must contain the following:</p><ul><li>use of at least 3 folds,</li><li>meet USPS First Class standards,</li><li>appropriately sized envelope,</li><li>include RSVP and RSVP envelope,</li><li>digital files completed with appropriate indicators, layers, file types, color mode, resolution.</li></ul></div>",
+        solution: '<div><h3>Creative Brief: </h3><p>The American Recorder Society is celebrating its 75 year anniversary as an active organization. To celebrate, the ARS will be having a Gala dinner followed by a concert by the Baroque chamber orchestra of Colorado, and featured soloist Pal Leenhoust.</p></div>'
+    },
+    {
+         name: 'Gift Certificate',
+        pictures: [],
+        problem: '<div><h3 class="objective">Objective:</h3><p>Redesign a printed massage therapy gift certificate.</p></div>',
+        client: '<div><h3>Client:</h3><p>Downtown Chiropractic and Rehabilitation, Fredericksburg,VA</p></div>',
+        solution: 'Solution 2'
+
+    }
+];
